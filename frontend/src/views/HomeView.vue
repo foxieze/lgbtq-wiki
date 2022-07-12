78f69e4bff4b9c2ed057f10d517c3c90c7e253c5
foxieze <jonah@rowlinson.eu>
2022-07-12 21:33:33 +0100

    add help message to home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,6 +4,15 @@
       <div class="hero-body">
         <h1 class="dictherotitle">Dictionary</h1>
         <h4 class="dictherosubtitle">Simple search for LGBTQIA+ vocabulary</h4>
+        <p>
+          If you don't understand any of this, I'd reccommend taking a look at
+          <router-link to="/help/meaning">some of the help pages</router-link>!
+          Or if you'd like help with something else,
+          <router-link to="/help/somethingelse"
+            >please check this out!</router-link
+          >
+          We're here to support you!
+        </p>
       </div>
     </div>
     <DictionaryComponent></DictionaryComponent>
@@ -15,10 +24,10 @@ import DictionaryComponent from "@/components/Dictionary.vue";
 export default {
   name: "HomeView",
   components: {
-    DictionaryComponent
+    DictionaryComponent,
   },
   mounted() {
-    document.title = "Home - LGBTQ Wiki"
-  }
+    document.title = "Home - LGBTQ Wiki";
+  },
 };
 </script>
